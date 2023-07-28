7dd98727256d8713f9cd68fbd243715e32fe58e8
Jhoglas Rocha <Jhoglas.Rocha-ext@ab-Inbev.com>
2023-07-28 08:52:03 -0300

    refactor

--- app/src/main/java/com/jhoglas/mysalon/ui/auth/Validator.kt
+++ app/src/main/java/com/jhoglas/mysalon/ui/auth/Validator.kt
@@ -5,71 +5,76 @@ import com.jhoglas.mysalon.utils.extensions.hasUppercase
 import com.jhoglas.mysalon.utils.extensions.isValidEmail
 
 object Validator {
-    fun validateFirstName(firstName: String): ValidateResult {
-        val rule = (firstName.isNullOrEmpty().not() && firstName.length >= 4)
-
-        return ValidateResult(
-            status = rule,
-            message = "Invalid field"
+    fun validateFirstName(firstName: String) = when {
+        firstName.isNullOrEmpty() -> ValidateResult(
+            status = false,
+            message = "First name is not empty",
         )
+
+        firstName.length < 1 -> ValidateResult(
+            status = false,
+            message = "First name must contain 1 or more digits",
+        )
+
+        else -> ValidateResult(status = true, message = "First name is valid")
     }
 
-    fun validateLastName(lastName: String): ValidateResult {
-        val rule = (lastName.isNullOrEmpty().not() && lastName.length >= 4)
-
-        return ValidateResult(
-            status = rule,
-            message = "Invalid field"
+    fun validateLastName(lastName: String) = when {
+        lastName.isNullOrEmpty() -> ValidateResult(
+            status = false,
+            message = "Last name is not empty",
         )
+
+        lastName.length < 1 -> ValidateResult(
+            status = false,
+            message = "Last name must contain 1 or more digits",
+        )
+
+        else -> ValidateResult(status = true, message = "Last name is valid")
     }
 
-    fun validateEmail(email: String): ValidateResult {
-        val rule = (email.isNullOrEmpty().not() && email.isValidEmail() && email.length >= 6)
-
-        return ValidateResult(
-            status = rule,
-            message = "Invalid email"
+    fun validateEmail(email: String) = when {
+        email.isNullOrEmpty() -> ValidateResult(
+            status = false,
+            message = "Email is not empty",
         )
+
+        email.isValidEmail().not() -> ValidateResult(
+            status = false,
+            message = "Invalid email",
+        )
+
+        else -> ValidateResult(status = true, message = "Email is valid")
     }
 
-    fun validatePassword(password: String): ValidateResult {
-        var isValid = ValidateResult()
+    fun validatePassword(password: String) = when {
+        password.isNullOrEmpty() -> ValidateResult(
+            status = false,
+            message = "Password is not empty",
+        )
 
-        if (password.isNullOrEmpty()) {
-            isValid = ValidateResult(
-                status = false,
-                message = "Password is not empty"
-            )
-        }
+        password.length < 6 -> ValidateResult(
+            status = false,
+            message = "Password must contain 6 or more digits",
+        )
 
-        if (password.length < 6) {
-            isValid = ValidateResult(
-                status = false,
-                message = "Password must contain 6 or more digits"
-            )
-        }
+        password.hasNumber().not() -> ValidateResult(
+            status = false,
+            message = "Password must contain one or more numbers",
+        )
 
-        if (password.hasNumber().not()) {
-            isValid = ValidateResult(
-                status = false,
-                message = "Password must contain one or more numbers"
-            )
-        }
+        password.hasUppercase().not() -> ValidateResult(
+            status = false,
+            message = "Password must contain one or more uppercase digits",
+        )
 
-        if (password.hasUppercase().not()) {
-            isValid = ValidateResult(
-                status = false,
-                message = "Password must contain one or more uppercase digits"
-            )
-        }
-
-        return isValid
+        else -> ValidateResult(status = true, message = "Password is valid")
     }
 
     fun validatePrivacyPolicyAcceptance(statusValue: Boolean): ValidateResult {
         return ValidateResult(
             status = statusValue,
-            message = "You must accept the privacy policy"
+            message = "You must accept the privacy policy",
         )
     }
 }
